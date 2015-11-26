a3a42614ecca267ae29030d3c3f15aaf8c23d855
Vlad Semenov <vladislav.semenov@gmail.com>
2015-11-27 04:24:56 +0600

    Added downloading of template release archive

--- bin/drug.js
+++ bin/drug.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log('Hello, console!');
+var request = require('request');
+var tar = require('tar');
+var zlib = require('zlib');
+
+var cwd = process.cwd();
+var templateUrl = 'https://github.com/semenov/drug-template/archive/v0.0.1.tar.gz';
+var outputDir = cwd;
+
+
+console.log('Downloading project template');
+
+request(templateUrl)
+    .pipe(zlib.Unzip())
+    .pipe(tar.Extract({ path: outputDir, strip: 1 }));
+
+
+
+
+
